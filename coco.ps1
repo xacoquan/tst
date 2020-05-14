56a2719d8369cdd48c808d93d08c3bb37be89691
xavier coquand <xacoquan@student.42.fr>
2020-05-14 14:42:43 +0200

    Add files via upload

--- coco.ps1
+++ coco.ps1
@@ -1,12 +1,12 @@
-function    Invoke-BloodHound{
+function    Invoke-coin{
           <#
           .SYNOPSIS
 
-                    Runs    the    BloodHound C#  Ingestor using   reflection.   The   assembly  is   stored    in this    file.
+                    Runs    the    coin C#  Ingestor using   reflection.   The   assembly  is   stored    in this    file.
 
           .DESCRIPTION
 
-                    Using   reflection    and    assembly.load, load    the    compiled   BloodHound    C# ingestor    into   memory
+                    Using   reflection    and    assembly.load, load    the    compiled   coin    C# ingestor    into   memory
                     and run it  without  touching disk.   Parameters    are    converted  to   the   equivalent    CLI    arguments
                     for the SharpHound  executable   and   passed    in via reflection. The appropriate function
                     calls   are   made  in   order to  ensure   that  assembly dependencies    are    loaded properly.
@@ -190,28 +190,28 @@ function    Invoke-BloodHound{
 
           .EXAMPLE
 
-                    PS  C:\> Invoke-BloodHound
+                    PS  C:\> Invoke-coin
 
                     Executes    the    default    collection options and exports JSONs   to    the    current    directory, compresses  the  data to  a    zip    file,
                     and then    removes    the    JSON   files from    disk
 
           .EXAMPLE
                     
-                    PS  C:\> Invoke-BloodHound   -Loop -LoopInterval   00:01:00  -LoopDuration    00:10:00
+                    PS  C:\> Invoke-coin   -Loop -LoopInterval   00:01:00  -LoopDuration    00:10:00
           
                     Executes    session    collection in  a    loop.  Will wait    1  minute   after each    run    to continue    collection
                     and will    continue   running   for   10    minutes    after  which    the    script will    exit
 
           .EXAMPLE
 
-                    PS  C:\> Invoke-BloodHound   -CollectionMethod All
+                    PS  C:\> Invoke-coin   -CollectionMethod All
           
                     Runs    ACL,   ObjectProps,  Container,   and   Default   collection    methods,   compresses    the    data   to    a  zip  file,
                     and then    removes    the    JSON   files from    disk
 
           .EXAMPLE    (Opsec!)
 
-                    PS  C:\> Invoke-BloodHound   -CollectionMethod DCOnly  --NoSaveCache    --RandomFilenames  --EncryptZip
+                    PS  C:\> Invoke-coin   -CollectionMethod DCOnly  --NoSaveCache    --RandomFilenames  --EncryptZip
           
                     Run LDAP    only   collection    methods    (Groups,   Trusts,   ObjectProps,  ACL, Containers, GPO Admins) without outputting  the  cache    file   to    disk.  
                     Randomizes  filenames    of the JSON    files  and  the  zip  file and adds    a  password to  the  zip  file
